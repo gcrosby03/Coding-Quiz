ab9d37187ccbaf2f749929deb8c9ed8d24f817ba
gcrosby03 <garrettcrosby.aas@gmail.com>
2022-09-06 15:42:02 -0500

    Started  html page and javascript page

---
+++ src/script.js
@@ -0,0 +1,64 @@
+var start_btn = document.querySelector(".start-btn")
+
+
+start_btn.addEventListener("click", function() {
+
+})
+
+var questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Multi Language",
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "insert question",
+        answer: "insert answer",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+          numb: 3,
+          question: "Your Question is Here",
+          answer: "Correct answer of the question is here",
+          options: [
+            "Option 1",
+            "option 2",
+            "option 3",
+            "option 4"
+          ]
+        },
+        {
+            numb: 4,
+            question: "Your Question is Here",
+            answer: "Correct answer of the question is here",
+            options: [
+              "Option 1",
+              "option 2",
+              "option 3",
+              "option 4"
+            ]
+          },
+          {
+            numb: 5,
+            question: "Your Question is Here",
+            answer: "Correct answer of the question is here",
+            options: [
+              "Option 1",
+              "option 2",
+              "option 3",
+              "option 4"
+            ]
+          },
+]
