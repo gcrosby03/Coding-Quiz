e4a7b61ddbca81f1a072ed6fd9d3961fdd824e02
gcrosby03 <garrettcrosby.aas@gmail.com>
2022-09-10 22:47:11 -0500

    Updated quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -3,3 +3,52 @@ var choices = Array.from(document.querySelector('.choice-text'));
 var progressText = document.querySelector('#progressText');
 var scoreText = document.querySelector('#score');
 var progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = []
+
+let questions = [
+    {
+        question: 'What is 2+2',
+        choice1: '2',
+        choice2: '4',
+        choice3: '7',
+        choice4: '6',
+        answer: 2,
+    },
+    {
+        question: 'What is 2+2',
+        choice1: '2',
+        choice2: '4',
+        choice3: '7',
+        choice4: '6',
+        answer: 2,
+    },
+    {
+        question: 'What is 2+2',
+        choice1: '2',
+        choice2: '4',
+        choice3: '7',
+        choice4: '6',
+        answer: 2,
+    },
+    {
+        question: 'What is 2+2',
+        choice1: '2',
+        choice2: '4',
+        choice3: '7',
+        choice4: '6',
+        answer: 2,
+    },
+    {
+        question: 'What is 2+2',
+        choice1: '2',
+        choice2: '4',
+        choice3: '7',
+        choice4: '6',
+        answer: 2,
+    }
+]
