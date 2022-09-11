1feca895d4758729867058c3701b347ac4a25c56
gcrosby03 <garrettcrosby.aas@gmail.com>
2022-09-10 22:37:00 -0500

    Made new html and css for the quiz and started js

---
+++ src/quiz.js
@@ -0,0 +1,5 @@
+var question = document.querySelector('#question');
+var choices = Array.from(document.querySelector('.choice-text'));
+var progressText = document.querySelector('#progressText');
+var scoreText = document.querySelector('#score');
+var progressBarFull = document.querySelector('#progressBarFull');
